73b82c745b0b55522a43713c3355eb8e033db609
dmitrykachala <kosas@mail.ru>
2022-04-11 12:58:25 +0300

    2. IndexControl. Таблица и вид. [#2092]

--- src/main/java/ru/job4j/accident/controller/IndexControl.java
+++ src/main/java/ru/job4j/accident/controller/IndexControl.java
@@ -3,7 +3,6 @@ package ru.job4j.accident.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.accident.repository.AccidentMem;
 import ru.job4j.accident.service.AccidentService;
 
 @Controller
@@ -11,8 +10,8 @@ public class IndexControl {
 
     private final AccidentService service;
 
-    public IndexControl(AccidentMem repo) {
-        service = new AccidentService(repo);
+    public IndexControl(AccidentService service) {
+        this.service = service;
     }
 
     @GetMapping("/")
